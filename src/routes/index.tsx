23f998cd5366eabbac25635987c93226a01ae92a
Vinícius Ventura <vini.ventura999@gmail.com>
2023-05-22 00:04:21 -0300

    removing not found page / route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
-import Booking from "../pages/Booking"; 
-import BookingList from "../pages/BookingList"; 
-import NotFound from "../pages/NotFound";
+import Booking from "../pages/Booking";
+import BookingList from "../pages/BookingList";
 
-function RoutesApp () {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/reservas" element={<Booking />}/>
-            <Route path="/bookingList" element={<BookingList />}/>
-            <Route path="*" element={<NotFound />} />
-        </Routes>
-    )
+function RoutesApp() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/reservas" element={<Booking />} />
+      <Route path="/bookingList" element={<BookingList />} />
+    </Routes>
+  );
 }
 
 export default RoutesApp;
